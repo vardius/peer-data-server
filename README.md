1822e2d184b6101785cf3a90ecab11a51753ee94
Rafał Lorenz <vardius@gmail.com>
2016-11-24 11:03:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PeerDataServer
+[![Build Status](https://travis-ci.org/Vardius/peer-data-server.svg?branch=master)](https://travis-ci.org/Vardius/peer-data)
+
+PeerDataServer is messaging service on *Node* using [socket.io](http://socket.io/). This is **ready to use** example signaling server for [PeerData](https://github.com/Vardius/peer-data) library.
+
+The sender and receiver *RTCPeerConnections* run in web pages on different devices, and we need a way for them to communicate metadata.
+For this, we use a signaling server: a server that can pass messages between *WebRTC* clients (peers).
+
+In order to set up and maintain a *WebRTC* call, *WebRTC* clients (peers) need to exchange metadata:
+- Candidate (network) information.
+- Offer and answer messages providing information about media.
+
+In other words, an exchange of metadata is required before peer-to-peer streaming of audio, video, or data can take place. This process is called signaling.
---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var io = require('socket.io')(8080),
+    uuid = require('node-uuid'),
+    os = require('os');
+
+const SocketEventType = {
+    CANDIDATE: 'candidate',
+    CONNECT: 'connect',
+    DISCONNECT: 'disconnect',
+    OFFER: 'offer',
+    ANSWER: 'answer'
+};
+
+io.sockets.on('connection', function (socket) {
+    var id = uuid.v4();
+
+    function log() {
+        socket.emit('log', ...arguments);
+    }
+
+    function onConnect() {
+        socket.join(id);
+    }
+
+    function onDisconnect() {
+        socket.leave(id);
+    }
+
+    socket.on('message', function (event) {
+        log('[Signaling Server]: ', event);
+
+        event.caller = {id: id};
+        if (event.callee) {
+            io.sockets.in(event.callee).emit('message', event);
+        } else {
+            switch (event.type) {
+                case SocketEventType.CONNECT:
+                    onConnect(event);
+                    break;
+                case SocketEventType.DISCONNECT:
+                    onDisconnect(event);
+                    break;
+            }
+            socket.broadcast.emit('message', event);
+        }
+    });
+
+    socket.on('ipaddr', function () {
+        var ifaces = os.networkInterfaces();
+        for (var dev in ifaces) {
+            ifaces[dev].forEach(function (details) {
+                if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+                    socket.emit('ipaddr', details.address);
+                }
+            });
+        }
+    });
+});
